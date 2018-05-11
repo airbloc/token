3262af24bd087d3810e100353e1a4d5fe13665d6
ByeongSu Hong <frostornge@gmail.com>
2018-05-11 23:21:54 +0900

    Done

--- contracts/PresaleSecond.sol
+++ contracts/PresaleSecond.sol
@@ -12,19 +12,14 @@ contract PresaleSecond is Ownable {
     using SafeMath for uint256;
     using SafeERC20 for ERC20;
 
-////////////////////////////////////
-//  constructor
-////////////////////////////////////
     uint256 public maxcap;      // sale hardcap
     uint256 public exceed;      // indivisual hardcap
     uint256 public minimum;     // indivisual softcap
     uint256 public rate;        // exchange rate
 
-    uint256 public startTime;     // sale startTime
-    uint256 public endTime;       // sale endTime
     bool public paused = false;   // is sale paused?
     bool public ignited = false;  // is sale started?
-    uint256 public weiRaised;     // check sale status
+    uint256 public weiRaised = 0;     // check sale status
 
     address public wallet;      // wallet for withdrawal
     address public distributor; // contract for release, refund
@@ -32,21 +27,16 @@ contract PresaleSecond is Ownable {
     ERC20 public Token;         // token
 
     constructor (
-        //////////////////////////
         uint256 _maxcap,
         uint256 _exceed,
         uint256 _minimum,
         uint256 _rate,
-        //////////////////////////
-        uint256 _startTime,
-        uint256 _endTime,
-        //////////////////////////
+
         address _wallet,
         address _distributor,
-        //////////////////////////
+
         address _whitelist,
         address _token
-        //////////////////////////
     )
         public
     {
@@ -55,21 +45,15 @@ contract PresaleSecond is Ownable {
         require(_distributor != address(0));
         require(_token != address(0));
 
-        require(_startTime > 0);
-        require(_endTime > 0);
-        require(_startTime < _endTime);
-
         maxcap = _maxcap;
         exceed = _exceed;
         minimum = _minimum;
         rate = _rate;
 
-        weiRaised = 0;
-        Token = ERC20(_token);
-
         wallet = _wallet;
         distributor = _distributor;
 
+        Token = ERC20(_token);
         List = Whitelist(_whitelist);
     }
 
@@ -78,32 +62,7 @@ contract PresaleSecond is Ownable {
         collect();
     }
 
-////////////////////////////////////
-//  time
-////////////////////////////////////
-    event startTimeChanged(uint256 _time);
-    event endTimeChanged(uint256 _time);
-
-    function setStartTime(uint256 _time) external onlyOwner {
-        require(!ignited);
-        require(_time > now);
-        require(_time < endTime);
-
-        startTime = _time;
-        emit startTimeChanged(_time);
-    }
-
-    function setEndTime(uint256 _time) external onlyOwner {
-        require(_time > now);
-        require(_time > startTime);
-
-        endTime = _time;
-        emit endTimeChanged(_time);
-    }
-
-////////////////////////////////////
 //  address
-////////////////////////////////////
     event Change(address _addr, string _name);
 
     function setWhitelist(address _whitelist) external onlyOwner {
@@ -128,9 +87,7 @@ contract PresaleSecond is Ownable {
         emit Change(_wallet, "wallet");
     }
 
-////////////////////////////////////
 //  sale controller
-////////////////////////////////////
     event Pause();
     event Unpause();
     event Ignite();
@@ -156,35 +113,30 @@ contract PresaleSecond is Ownable {
         emit Extinguish();
     }
 
-////////////////////////////////////
 //  collect eth
-////////////////////////////////////
-    event Purchase(address indexed _buyer, uint256 _price, uint256 _tokens);
+    event Purchase(address indexed _buyer, uint256 _purchased, uint256 _refund, uint256 _tokens);
 
     mapping (address => uint256) public buyers;
-    mapping (address => uint256) public indexes;
     address[] public keys;
 
-    function getKeyLength() external returns (uint256) {
+    function getKeyLength()
+        external
+        view
+        returns (uint256)
+    {
         return keys.length;
     }
 
-    /**
-     * @dev collect ether from buyer
-     */
     function collect() public payable {
         require(ignited && !paused);
-        require(List.whitelist[msg.sender]);
+        require(List.whitelist(msg.sender));
 
-        // prevent purchase delegation
         address buyer = msg.sender;
 
         require(buyer != address(0));
         require(buyers[buyer].add(msg.value) >= minimum);
         require(buyers[buyer] < exceed);
         require(weiRaised < maxcap);
-        require(now < endTime);
-        require(now > startTime);
 
         if(buyers[buyer] == 0) keys.push(buyer);
 
@@ -193,37 +145,17 @@ contract PresaleSecond is Ownable {
 
         (purchase, refund) = getPurchaseAmount(buyer);
 
-        // buy
-        uint256 tokenAmount = purchase.mul(rate);
         weiRaised = weiRaised.add(purchase);
 
         if(weiRaised >= maxcap) ignited = false;
 
-        // wallet
         buyers[buyer] = buyers[buyer].add(purchase);
-        emit Purchase(buyer, purchase, tokenAmount);
+        emit Purchase(buyer, purchase, refund, purchase.mul(rate));
 
-        // refund
         buyer.transfer(refund);
     }
 
-////////////////////////////////////
 //  util functions for collect
-////////////////////////////////////
-    /**
-     * D1 = 세일총량 - 세일판매량
-     * D2 = 개인최대 - 선입금량
-     * 환불량 = 입금량 - MIN D1, D2
-     * if 환불량 < 0
-     *      return [ 다샀음! ]
-     * else
-     *      return [ 조금 사고 환불! ]
-     */
-
-    /**
-     * @dev get amount of buyer can purchase
-     * @param _buyer The address that tries to purchase
-     */
     function getPurchaseAmount(address _buyer)
         private
         view
@@ -242,44 +174,32 @@ contract PresaleSecond is Ownable {
 
     function min(uint256 _a, uint256 _b)
         private
-        view
+        pure
         returns (uint256)
     {
         return (_a > _b) ? _b : _a;
     }
 
-////////////////////////////////////
 //  finalize
-////////////////////////////////////
-    /**
-     * @dev if sale finalized?
-     */
     bool public finalized = false;
 
-    /**
-     * @dev finalize sale and withdraw everything (token, ether)
-     */
     function finalize() external onlyOwner {
         require(!ignited && !finalized);
 
-        // send ether and token to dev wallet
         withdrawEther();
         withdrawToken();
 
         finalized = true;
     }
 
-////////////////////////////////////
 //  release & release
-////////////////////////////////////
     event Release(address indexed _to, uint256 _amount);
     event Refund(address indexed _to, uint256 _amount);
 
-    /**
-     * @dev release token to buyer
-     * @param _addr The address that owner want to release token
-     */
-    function release(address _addr) external returns (bool) {
+    function release(address _addr)
+        external
+        returns (bool)
+    {
         require(!ignited && !finalized);
         require(msg.sender == distributor); // only for distributor
         require(_addr != address(0));
@@ -298,7 +218,10 @@ contract PresaleSecond is Ownable {
      * @dev refund ether to buyer
      * @param _addr The address that owner want to refund ether
      */
-    function refund(address _addr) external returns (bool) {
+    function refund(address _addr)
+        external
+        returns (bool)
+    {
         require(!ignited && !finalized);
         require(msg.sender == distributor); // only for distributor
         require(_addr != address(0));
