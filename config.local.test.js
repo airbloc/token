5ec97cbadfa4e3258000ecbd598de2b9dcd34653
ByeongSu Hong <frostornge@gmail.com>
2018-05-23 11:29:42 +0900

    Delete config.local.test.js

--- config.local.test.js
+++
@@ -1,7 +0,0 @@
-const HDWalletProvider = require('truffle-hdwallet-provider')
-const mnemonic = 'airbloc frostornge airbloc frostornge airbloc frostornge airbloc frostornge airbloc frostornge airbloc frostornge'
-const key = 'AiRbLoCfRoStOrNgE'
-
-module.exports = (network) => {
-    return new HDWalletProvider(mnemonic, `https://${network}.infura.io/${key}`)
-}
